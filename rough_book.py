d2602fb5e93fd7771d570ec33d4c3a5556eec35d
Ashutosh Vishwakarma <karmav44990@gmail.com>
2021-04-11 18:18:38 +0530

    code for convering daily ohlc to csv

--- rough_book.py
+++ rough_book.py
@@ -1,68 +1,35 @@
-import pickle
-from datetime import datetime
-from kite_enums import Variety, Exchange, TransactionType, PRODUCT, OrderType, VALIDITY
-# from stock_trade_script import *
-from stock_trade_script import MainClass
-
-
-def refresh_id(line):
-    return line.split(",")[0].replace("refresh_id: ", '')
-
-
-def action_type(line):
-    if 'Starting' in line:
-        return 'start'
-    return 'end'
-
-
-def time(line):
-    tstr = line.split("at ")[1]
-    if "." in tstr:
-        tstr = tstr.split(".")[0]
-    return datetime.strptime(tstr, '%Y-%m-%d %H:%M:%S')
-
-
-def process_line(line):
-    return tryf(refresh_id, line), tryf(action_type, line), tryf(time, line)
-
-
-def tryf(func, arg):
-    try:
-        return func(arg)
-    except Exception as e:
-        return None
-
-
-def get_bse_news_crawler_performance():
-    lines = [line.strip() for line in open("./bse.log").readlines()]
-    processed_lines = [process_line(line) for line in lines]
-    result = {}
-    for line in processed_lines:
-        uid, action, action_time = line[0], line[1], line[2]
-        if uid not in result.keys():
-            result[uid] = {}
-
-        if action in result[uid].keys():
-            raise Exception("duplicate uid action found {} {}".format(uid, action))
-
-        result[uid][action] = action_time
-
-    for v in result.values():
-        v['diff'] = v['end'] - v['start']
-    return result
-
+import json
 
 if __name__ == '__main__':
 
-    mc = MainClass()
-    kite = mc._kite_connect
-    kite.place_order(variety=Variety.BRACKET.value,
-                     exchange=Exchange.BSE.value,
-                     tradingsymbol='ICICIBANK',
-                     transaction_type=TransactionType.BUY.value,
-                     quantity=1,
-                     product=PRODUCT.MIS.value,
-                     order_type=OrderType.LIMIT.value,
-                     validity=VALIDITY.DAY.value,
-                     squareoff=3,
-                     stoploss=2.0, trailing_stoploss=1.0, price=470)
+    file_names = ['ADANIPORTS_3861249.json', 'ASIANPAINT_60417.json', 'BAJAJ-AUTO_4267265.json', 'BAJAJFINSV_4268801.json',
+                  'BHARTIARTL_2714625.json', 'BPCL_134657.json', 'BRITANNIA_140033.json', 'CIPLA_177665.json',
+                  'DIVISLAB_2800641.json', 'DRREDDY_225537.json', 'EICHERMOT_232961.json', 'GRASIM_315393.json',
+                  'HCLTECH_1850625.json', 'HDFC_340481.json', 'HDFCBANK_341249.json', 'HEROMOTOCO_345089.json',
+                  'HINDPETRO_359937.json', 'HINDUNILVR_356865.json', 'INDUSINDBK_1346049.json', 'INFY_408065.json',
+                  'IOC_415745.json', 'ITC_424961.json', 'JSWSTEEL_3001089.json', 'KOTAKBANK_492033.json', 'LT_2939649.json',
+                  'MARUTI_2815745.json', 'M&M_519937.json', 'NESTLEIND_4598529.json', 'NTPC_2977281.json',
+                  'ONGC_633601.json', 'POWERGRID_3834113.json', 'RELIANCE_738561.json', 'SAIL_758529.json',
+                  'SBILIFE_5582849.json', 'SBIN_779521.json', 'SHREECEM_794369.json', 'SUNPHARMA_857857.json',
+                  'TATACONSUM_878593.json', 'TATAMOTORS_884737.json', 'TECHM_3465729.json', 'TITAN_897537.json',
+                  'ULTRACEMCO_2952193.json', 'UPL_2889473.json', 'WIPRO_969473.json']
+
+    folders = ['2015_16', '2016_17', '2017_18', '2018_19', '2019_20', '2020_21', '2021']
+    json_base_path = '/data/kite_websocket_data/historical/'
+    csv_base_path = '/data/kite_websocket_data/historical/csv_files/'
+
+    for folder in folders:
+        for file_name in file_names:
+            json_file_path = json_base_path + folder + "/" + file_name
+            output_file_path = csv_base_path + folder + "/" + file_name.replace(".json", ".csv")
+            with open(json_file_path) as handle:
+                arr = json.load(handle)
+
+            header = "date,open,high,low,close,volume\n"
+            rows = [",".join([str(e) for e in elem]) + "\n" for elem in arr]
+            f = open(output_file_path, 'w')
+            f.write(header)
+            for row in rows:
+                f.write(row)
+            f.close()
+
