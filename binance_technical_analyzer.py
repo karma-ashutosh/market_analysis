132e10f0ea8c4226780e6c4c119c9067b18faf91
Ashutosh Vishwakarma <karmav44990@gmail.com>
2021-04-19 22:34:15 +0530

    Fixed preapred data date format

--- binance_technical_analyzer.py
+++ binance_technical_analyzer.py
@@ -9,7 +9,9 @@ if __name__ == '__main__':
     def timestamp_to_date_string(timestamp_millis):
         if not timestamp_millis:
             return None
-        d = datetime.fromtimestamp(timestamp_millis / 1000, tz=timezone.utc)
+        ist_forward = (5 * 60 + 30) * 60 * 1000
+        ist_millis = timestamp_millis + ist_forward
+        d = datetime.fromtimestamp(ist_millis / 1000, tz=timezone.utc)
         return d.strftime(format=Constants.strp_time_format1)
 
     def file_to_provider_func(file_path):
@@ -18,8 +20,9 @@ if __name__ == '__main__':
     read_file_path = BINANCE.DATA_FILE_READ_BASE_PATH
     write_file_path = BINANCE.DATA_FILE_WRITE_BASE_PATH
     file_paths = FilePaths(read_file_path, write_file_path, [BINANCE.SYMBOL + ".json"], [BINANCE.SYMBOL])
-    save_predicted_trades_and_summary(file_paths, 2021, 1, 3, file_to_provider_func,
-                                      date_converter=timestamp_to_date_string)
+    save_predicted_trades_and_summary(file_paths, 2021, 1, 3, file_to_provider_func
+                                      , date_converter=timestamp_to_date_string)
+                                      # )
 
     # daily_indicatory = DailyMovingAvgIndicator(0, 1, 3, file_paths, file_to_provider_func)
     # daily_indicatory.filter_func = lambda _: True
